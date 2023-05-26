ad5082855d0b887d9e1931217fe0e57d2415eb5c
sbassim <sleiman.bassim@plainconcepts.com>
2023-05-26 11:03:13 +0300

    remove iam check

--- main.tf
+++ main.tf
@@ -80,20 +80,14 @@ data "aws_iam_policy_document" "iam_for_lambda_policy" {
 }
 
 
-## Check if IAM role exist, use it if it does
-data "aws_iam_role" "existing" {
-  name = "iam_for_lambda"
-}
-
-resource "aws_iam_role" "new" {
-  count              = try(data.aws_iam_role.existing.name, "iam_for_lambda") == "iam_for_lambda" ? 0 : 1
+resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
 resource "aws_iam_role_policy" "iam_for_lambda_policy" {
   name   = "iam_for_lambda_policy"
-  role   = try(data.aws_iam_role.existing.id, aws_iam_role.new[0].id)
+  role   = aws_iam_role.iam_for_lambda.id
   policy = data.aws_iam_policy_document.iam_for_lambda_policy.json
 }
 
@@ -150,7 +144,7 @@ resource "aws_security_group" "lambda_sg_prod" {
 
 resource "aws_lambda_function" "app_server_testing_dev" {
   function_name = "lambda_test_function_dev"
-  role          = try(data.aws_iam_role.existing.arn, aws_iam_role.new[0].arn)
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
   vpc_config {
@@ -170,7 +164,7 @@ resource "aws_lambda_function" "app_server_testing_dev" {
 
 resource "aws_lambda_function" "app_server_testing_prod" {
   function_name = "lambda_test_function_prod"
-  role          = try(data.aws_iam_role.existing.arn, aws_iam_role.new[0].arn)
+  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.10"
   vpc_config {
